ff13ae72aeeb5a6d60d94aee916644cd8138c909
Ali Ghorashi <aligho@mit.edu>
2022-12-02 13:46:53 -0500

    shifted lattices

---
+++ symeigs/shiftedlattices/input/dim2-sg9-2-res64-tm.sh
@@ -0,0 +1,17 @@
+run-type="tm"
+dim=2
+sgnum=9
+res=64
+origin=(vector3 -0.5 0.5)
+rvecs=(list (vector3 0.5 -0.32910221158395797) (vector3 0.5 0.32910221158395797))
+uc-gvecs=(list (vector3 0 -1) (vector3 -1 0) (vector3 1 0) (vector3 0 1) (vector3 -1 -1) (vector3 1 1) (vector3 1 -1) (vector3 -1 1) (vector3 0 -2) (vector3 -2 0) (vector3 2 0) (vector3 0 2) (vector3 -1 -2) (vector3 -2 -1) (vector3 2 1) (vector3 1 2) (vector3 1 -2) (vector3 -2 1) (vector3 2 -1) (vector3 -1 2) (vector3 -2 -2) (vector3 2 2) (vector3 2 -2) (vector3 -2 2) (vector3 0 -3) (vector3 -3 0) (vector3 3 0) (vector3 0 3) (vector3 -1 -3) (vector3 -3 -1) (vector3 3 1) (vector3 1 3) (vector3 1 -3) (vector3 -3 1) (vector3 3 -1) (vector3 -1 3) (vector3 -2 -3) (vector3 -3 -2) (vector3 3 2) (vector3 2 3) (vector3 2 -3) (vector3 -3 2) (vector3 3 -2) (vector3 -2 3) (vector3 0 -4) (vector3 -4 0) (vector3 4 0) (vector3 0 4) (vector3 -1 -4) (vector3 -4 -1) (vector3 4 1) (vector3 1 4) (vector3 1 -4) (vector3 -4 1) (vector3 4 -1) (vector3 -1 4) (vector3 -3 -3) (vector3 3 3) (vector3 3 -3) (vector3 -3 3) (vector3 -2 -4) (vector3 -4 -2) (vector3 4 2) (vector3 2 4) (vector3 2 -4) (vector3 -4 2) (vector3 4 -2) (vector3 -2 4) (vector3 0 -5) (vector3 -5 0) (vector3 5 0) (vector3 0 5) (vector3 -1 -5) (vector3 -5 -1) (vector3 5 1) (vector3 1 5) (vector3 1 -5) (vector3 -5 1) (vector3 5 -1) (vector3 -1 5) (vector3 0 -6) (vector3 -6 0) (vector3 6 0) (vector3 0 6))
+uc-coefs=(list -0.0092+0.0i -0.0092+0.0i -0.0092+0.0i -0.0092+0.0i -0.0027+0.0i -0.0027+0.0i -0.0184+0.0i -0.0184+0.0i -0.0048+0.0i -0.0048+0.0i -0.0048+0.0i -0.0048+0.0i -0.0105+0.0i -0.0105+0.0i -0.0105+0.0i -0.0105+0.0i 0.0032+0.0i 0.0032+0.0i 0.0032+0.0i 0.0032+0.0i -0.0009+0.0i -0.0009+0.0i 0.0028+0.0i 0.0028+0.0i 0.0036+0.0i 0.0036+0.0i 0.0036+0.0i 0.0036+0.0i -0.0077+0.0i -0.0077+0.0i -0.0077+0.0i -0.0077+0.0i -0.002+0.0i -0.002+0.0i -0.002+0.0i -0.002+0.0i 0.0099+0.0i 0.0099+0.0i 0.0099+0.0i 0.0099+0.0i -0.007+0.0i -0.007+0.0i -0.007+0.0i -0.007+0.0i -0.0068+0.0i -0.0068+0.0i -0.0068+0.0i -0.0068+0.0i -0.0042+0.0i -0.0042+0.0i -0.0042+0.0i -0.0042+0.0i 0.0067+0.0i 0.0067+0.0i 0.0067+0.0i 0.0067+0.0i -0.0007+0.0i -0.0007+0.0i -0.0059+0.0i -0.0059+0.0i 0.0027+0.0i 0.0027+0.0i 0.0027+0.0i 0.0027+0.0i -0.0034+0.0i -0.0034+0.0i -0.0034+0.0i -0.0034+0.0i 0.0019+0.0i 0.0019+0.0i 0.0019+0.0i 0.0019+0.0i -0.0005+0.0i -0.0005+0.0i -0.0005+0.0i -0.0005+0.0i -0.0033+0.0i -0.0033+0.0i -0.0033+0.0i -0.0033+0.0i -0.0012+0.0i -0.0012+0.0i -0.0012+0.0i -0.0012+0.0i)
+uc-level=0.015122052848067993
+epsin=8
+epsout=1.0
+prefix="dim2-sg9-2-res64-tm"
+nbands=40
+Ws=(list (matrix3x3 (vector3 1.0 0.0 0.0) (vector3 0.0 1.0 0.0) (vector3 0.0 0.0 1.0)) (matrix3x3 (vector3 -1.0 0.0 0.0) (vector3 0.0 -1.0 0.0) (vector3 0.0 0.0 1.0)) (matrix3x3 (vector3 0.0 -1.0 0.0) (vector3 -1.0 0.0 0.0) (vector3 0.0 0.0 1.0)) (matrix3x3 (vector3 0.0 1.0 0.0) (vector3 1.0 0.0 0.0) (vector3 0.0 0.0 1.0)))
+ws=(list (vector3 0.0 0.0) (vector3 0.0 0.0) (vector3 0.0 0.0) (vector3 0.0 0.0))
+opidxs=(list (list 1 2 3 4) (list 1 2) (list 1 2 3 4))
+kvecs=(list (vector3 0.5 0.5) (vector3 0.0 0.5) (vector3 0.0 0.0))
