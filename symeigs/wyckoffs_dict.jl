58abdac3a895be7b9df353b94bfbf768308557a2
Ali Ghorashi <aligho@mit.edu>
2022-12-05 18:41:34 -0500

    put wyckoffs dict in separate julia file to not repeat code

---
+++ symeigs/wyckoffs_dict.jl
@@ -0,0 +1,7 @@
+using Crystalline, MPBUtils, JLD2, LinearAlgebra, StaticArrays, SymmetryBases, PyPlot
+
+sg_wyckoffs = [filter(x -> x.mult == 1, wyckoffs(i, 2)) for i in (2, 6, (9:17)...)]
+sg_wyckoffs[3] = wyckoffs(9, 2)[end-1:end];
+sg_wyckoffs[6] = [wyckoffs(12, 2)[end]];
+sg_wyckoffs[9] = [WyckoffPosition(1, 'a', RVec([0, 0])), WyckoffPosition(2, 'b', RVec([1/3, 2/3])), WyckoffPosition(2, 'c', RVec([2/3, 1/3]))]
+sg_wyckoffs_dict = Dict(([2, 6, 9:17...])[i] => sg_wyckoffs[i] for i in 1:11)
