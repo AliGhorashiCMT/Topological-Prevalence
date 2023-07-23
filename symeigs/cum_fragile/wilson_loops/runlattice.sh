a83edcf452f02c84629cb3496a34488860dd71d3
Ali Ghorashi <aligho@mit.edu>
2023-07-23 13:35:27 -0400

    Got rid of mpi warnings for wilson loop nodal points

---
+++ symeigs/cum_fragile/wilson_loops/runlattice.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+calcname=$1
+export OPENBLAS_NUM_THREADS=1
+IFS=$'\n';
+export calcname
+echo "calcname is: $calcname"
+#mpirun --quiet mpb-mpi $(cat input/${calcname}.sh) \
+#fourier-lattice.ctl 2>&1 | tee logs/${calcname}.log
+mpirun --quiet mpb-mpi $(cat input/${calcname}.sh) $(cat ./input/kpoints.sh) \
+fourier-lattice.ctl 2>&1 | tee logs/${calcname}.log
+unset IFS;
+runtype=$(grep "run-type=" input/${calcname}.sh | sed 's/run-type=//;s/\"//g') # get polarization-string
+res=$(grep "res=" input/${calcname}.sh | sed 's/res=//') # get resolution
+. get-berry-phases.sh ${calcname}
