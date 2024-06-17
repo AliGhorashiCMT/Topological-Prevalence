a1c7072305e753e90a025fd3eafe6ef72634ff76
Ali Ghorashi <aligho@mit.edu>
2024-06-17 17:39:03 -0400

    Julia version of get-freqs.sh and get-symeigs.sh (So we can get rid of .out files

---
+++ get-freqs-symeigs.jl
@@ -0,0 +1,14 @@
+function logs_to_dispersion(logstr::String, mode::String)
+    split_log = split(logstr, "\n");
+    filtered_split_log = filter(x -> startswith(x, "$(mode)freqs"), split_log)[2:end]
+    filtered_split_log = replace.(filtered_split_log, "$(mode)freqs:, " => "", " " =>"")
+    filtered_split_log = [x * "\n" for x in filtered_split_log]
+    return prod(filtered_split_log)
+end;
+function logs_to_symeigs(logstr::String)
+    split_log = split(logstr, "\n");
+    filtered_split_log = filter(x -> startswith(x, "sym-eigs:"), split_log)
+    filtered_split_log = replace.(filtered_split_log, "sym-eigs:, " => "", " " =>"", "("=>"\"", ")"=>"\"")
+    filtered_split_log = [x * "\n" for x in filtered_split_log]
+    return prod(filtered_split_log)
+end;
