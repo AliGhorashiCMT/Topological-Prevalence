160c897324876766c697757a7f389eb777201d8c
Ali Ghorashi <aligho@mit.edu>
2024-06-18 13:08:42 -0400

    Resolution sweep code

--- Revisions/Resolution_Sweep/runlattice.sh
+++ Revisions/Resolution_Sweep/runlattice.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
-calcname=$1
+inputdata=$1
 sg=$2
 epsid=$3
-residx=$4
-swresolution=$5
-savename=$6
+epsin=$4
+runtype=$5
+residx=$6
+swresolution=$7
+savename=$8
 export OPENBLAS_NUM_THREADS=1
 IFS=$'\n';
 export calcname
 export swresolution
 export savename
-echo "calcname is: $calcname"
+
+#echo "calcname is: $calcname"
 echo "calculation is being solved in ${savename}"
 echo "real resolution is ${swresolution}"
-mpb $(cat ../../symeigs/input/${calcname}.sh) res=${swresolution} \
+
+#mpb $(cat ../../symeigs/input/${calcname}.sh) res=${swresolution} \
+#fourier-lattice.ctl 2>&1 | tee logs/${savename}.log
+
+# Same but now with JLD2 file data
+
+mpb ${inputdata} epsin=${epsin} res=${swresolution} \
 fourier-lattice.ctl 2>&1 | tee logs/${savename}.log
+
 unset IFS;
-runtype=$(grep "run-type=" ../../symeigs/input/${calcname}.sh | sed 's/run-type=//;s/\"//g') # get polarization-string
+
+#runtype=$(grep "run-type=" ../../symeigs/input/${calcname}.sh | sed 's/run-type=//;s/\"//g') # get polarization-string
 cat logs/${savename}.log | . get-freqs.sh $runtype ${savename}-dispersion.out
 cat logs/${savename}.log | . get-symeigs.sh ${savename}-symeigs.out
 
--- Revisions/Resolution_Sweep/runlattices-array.jl
+++ Revisions/Resolution_Sweep/runlattices-array.jl
@@ -1,15 +1,38 @@
 task_id, num_tasks = parse.(Int, ARGS)
 println("task_id is $task_id")
 println("num_tasks $num_tasks")
-ids = 1:1000
+
+flush(stdout)
+
+using JLD2
+ids = 1:10000
+epses = [8, 12, 16, 24, 32]
 eps_id = 3
+resolutionv = [8, 12, 16, 24, 32, 48, 128, 256]
+#resolutionv = [8, 12, 16, 24, 32, 48, 128]
+eps = epses[eps_id]
 for sgnum in [2, 10, 13, 16]
 	for mode in ["te", "tm"]
+		dir = "../../symeigs/input/"
+		loaded_datav = load(dir*"sg$sgnum-epsid1-res64-$mode-input.jld2")["inputsv"]
 		for id in task_id:num_tasks:ids[end]
-			for (res_idx, resolution) in enumerate([8, 12, 16, 24, 32, 48])
-				real_id = id + (eps_id - 1)*10000
+			loaded_data = loaded_datav[id]
+			for (res_idx, resolution) in enumerate(resolutionv)
 				println("\n\n")
-				run(`./runlattice.sh dim2-sg$sgnum-$(real_id)-res64-$mode $(sgnum) $(eps_id) $(res_idx) $(resolution) dim2-sg$sgnum-$(real_id)-res$(resolution)-$mode`)
+				real_id = id + (eps_id - 1)*10000
+				symeigs_dispersions_dir = "./output/sg$(sgnum)/eps$(eps_id)/$(mode)/res$(res_idx)/"
+				savename = "dim2-sg$(sgnum)-$(real_id)-res$(resolution)-$(mode)"
+				symeigs_filename = symeigs_dispersions_dir*savename*"-symeigs.out"; dispersions_filename = symeigs_dispersions_dir*savename*"-dispersion.out"
+				if (isfile(symeigs_filename) && isfile(dispersions_filename))
+					println("Detected that calculation for spacegroup $(sgnum) for id: $(real_id), mode $(mode) and resolution $(resolution) exists. Skipping...");
+					flush(stdout)
+					continue
+				end
+				println("Running calculation for spacegroup $(sgnum) for id: $(real_id), mode $(mode) and resolution $(resolution): ")
+				#resolution > 100 || continue # Run the new resolutions
+				run(`./runlattice.sh $(loaded_data) $(sgnum) $(eps_id) $(eps) $(mode) $(res_idx) $(resolution) $(savename)`)
+				#run(`./runlattice.sh dim2-sg$sgnum-$(real_id)-res64-$mode
+				 #$(sgnum) $(eps_id) $(res_idx) $(resolution) dim2-sg$sgnum-$(real_id)-res$(resolution)-$mode`)
 			end 
 		end
 	end
